68e68af8b45485e2927091aa1e66093eda29189e
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2024-05-08 14:01:36 +0100

    Implements fixed sampler

--- src/envs/env_utils.py
+++ src/envs/env_utils.py
@@ -1,4 +1,4 @@
-from typing import Type
+from typing import Type, Callable
 
 from gymnasium.wrappers import FlattenObservation, TimeLimit
 
@@ -6,31 +6,30 @@ from envs.alternate_wrapper import AlternateWrapper
 from envs.dict_wrapper import DictWrapper
 from envs.ldba_graph_wrapper import LDBAGraphWrapper
 from envs.ltl_goal_wrapper import LTLGoalWrapper
-from envs.goal_index_wrapper import GoalIndexWrapper
 from envs.remove_trunc_wrapper import RemoveTruncWrapper
 
 from ltl import LTLSampler
 from ltl.logic import FrozenAssignment
 
 
-def make_env(name: str, ltl_sampler: Type[LTLSampler], render_mode: str | None = None):
+def make_env(name: str, make_sampler: Callable[[list[str]], LTLSampler], render_mode: str | None = None):
     if name.startswith('pretraining_'):
         underlying = name[len('pretraining_'):]
-        underlying_env = make_env(underlying, ltl_sampler, render_mode)
+        underlying_env = make_env(underlying, make_sampler, render_mode)
         propositions = underlying_env.get_wrapper_attr('get_propositions')()
         impossible_assignments = underlying_env.get_wrapper_attr('get_impossible_assignments')()
-        return make_pretraining_env(propositions, impossible_assignments, ltl_sampler)
+        return make_pretraining_env(propositions, impossible_assignments, make_sampler)
     if is_safety_gym_env(name):
-        return make_safety_gym_env(name, ltl_sampler, render_mode)
+        return make_safety_gym_env(name, make_sampler, render_mode)
     else:
-        return make_dmc_env(name, ltl_sampler, render_mode)
+        return make_dmc_env(name, make_sampler, render_mode)
 
 
 def is_safety_gym_env(name: str) -> bool:
     return any([name.startswith(agent_name) for agent_name in ['Point', 'Car', 'Racecar', 'Doggo', 'Ant']])
 
 
-def make_safety_gym_env(name: str, ltl_sampler: Type[LTLSampler], render_mode: str | None = None):
+def make_safety_gym_env(name: str, ltl_sampler: Callable[[list[str]], LTLSampler], render_mode: str | None = None):
     # noinspection PyUnresolvedReferences
     import safety_gymnasium
     from envs.zones.safety_gym_wrapper import SafetyGymWrapper
@@ -45,7 +44,7 @@ def make_safety_gym_env(name: str, ltl_sampler: Type[LTLSampler], render_mode: s
     return env
 
 
-def make_dmc_env(name: str, ltl_sampler: Type[LTLSampler], render_mode: str | None = None):
+def make_dmc_env(name: str, ltl_sampler: Callable[[list[str]], LTLSampler], render_mode: str | None = None):
     # noinspection PyUnresolvedReferences
     from dm_control import suite, viewer
     import envs.dmc as dmc
@@ -71,7 +70,7 @@ def make_dmc_env(name: str, ltl_sampler: Type[LTLSampler], render_mode: str | No
 def make_pretraining_env(
         propositions: set[str],
         impossible_assignments: set[FrozenAssignment],
-        ltl_sampler: Type[LTLSampler]
+        ltl_sampler: Callable[[list[str]], LTLSampler]
 ):
     from envs.pretraining.pretraining_env import PretrainingEnv
 
