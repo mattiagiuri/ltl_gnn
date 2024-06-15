dc03ebbd4d519aafe910b2908bdcab6c199e783d
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2024-06-15 19:21:34 +0100

    Updates samplers

--- src/sequence/random_sequence_sampler.py
+++ src/sequence/random_sequence_sampler.py
@@ -1,22 +1,38 @@
 import random
 from pprint import pprint
+from typing import Callable
 
 
 class RandomSequenceSampler:  # TODO: rewrite using assignments instead of simply propositions
-    def __init__(self, propositions: list[str]):
+
+    @classmethod
+    def partial(cls, length: int, unique: bool) -> Callable[[list[str]], 'RandomSequenceSampler']:
+        return lambda propositions: cls(propositions, length, unique)
+
+    def __init__(self, propositions: list[str], length: int, unique: bool):
         self.propositions = sorted(propositions)
+        self.length = length
+        self.unique = unique
         self.is_adaptive = False
+
+    def __call__(self):
+        if self.unique:
+            return self.sample_unique(self.length)
+        return self.sample(self.length)
 
     def sample(self, length: int) -> list[tuple[str, str]]:
         seq = [tuple(random.sample(self.propositions, 2))]
-        seq[0] = (seq[0][0], 'empty')
         for i in range(1, length):
             last_reach, last_avoid = seq[-1]
             reach, avoid = random.sample(self.propositions, 2)
             while reach == last_reach or avoid == last_reach:
                 reach, avoid = random.sample(self.propositions, 2)
-            seq.append((reach, 'empty'))
+            seq.append((reach, avoid))
         return seq
+
+    def sample_unique(self, length: int) -> list[tuple[str, str]]:
+        seq = random.sample(self.propositions, 2 * length)
+        return [(seq[i], seq[i + 1]) for i in range(0, 2 * length, 2)]
 
 
 if __name__ == '__main__':
