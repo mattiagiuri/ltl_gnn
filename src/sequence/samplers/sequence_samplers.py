7a74b38109ce4f9ce14f147251f5e37be607b104
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2024-07-15 21:28:15 +0100

    Fixes sequence sampler

--- src/sequence/samplers/sequence_samplers.py
+++ src/sequence/samplers/sequence_samplers.py
@@ -1,4 +1,5 @@
 import random
+from pprint import pprint
 from typing import Callable
 
 from ltl.automata import LDBASequence
@@ -9,16 +10,22 @@ def sample_reach_avoid(
         depth: int | tuple[int, int],
         num_reach: int | tuple[int, int],
         num_avoid: int | tuple[int, int],
+        not_reach_same_as_last: bool = False
 ) -> Callable[[list[str]], LDBASequence]:
     def wrapper(propositions: list[str]) -> LDBASequence:
         def sample_one(last_reach: set[str]):
             nr = random.randint(*num_reach) if isinstance(num_reach, tuple) else num_reach
             na = random.randint(*num_avoid) if isinstance(num_avoid, tuple) else num_avoid
-            available = [p for p in propositions if p not in last_reach]
+            available = [p for p in propositions if p not in last_reach] if not_reach_same_as_last else propositions
             reach = random.sample(available, nr)
-            available = [p for p in available if p not in reach]
+            available = [p for p in propositions if p not in reach and p not in last_reach]
+            if len(available) < na:
+                if isinstance(num_avoid, tuple):
+                    na = random.randint(num_avoid[0], len(available)) if num_avoid[0] < len(available) else len(
+                        available)
+                else:
+                    raise ValueError('Not enough propositions to sample from')
             avoid = random.sample(available, na)
-            assert not (set(reach) & set(avoid) or set(reach) & set(last_reach) or set(avoid) & set(last_reach))
             reach_assignments = frozenset([Assignment.single_proposition(p, propositions).to_frozen() for p in reach])
             avoid_assignments = frozenset([Assignment.single_proposition(p, propositions).to_frozen() for p in avoid])
             return reach_assignments, avoid_assignments, reach
@@ -49,9 +56,9 @@ def all_reach_avoid_tasks(depth: int) -> Callable[[list[str]], list[LDBASequence
             rec_res = rec(depth - 1)
             result = []
             for task in rec_res:
-                next_reach = task[0][0]
+                next_reach, next_avoid = task[0]
                 for p, q in reach_avoids:
-                    if p == next_reach or q == next_reach:
+                    if p == next_reach or p == next_avoid:
                         continue
                     result.append(((p, q),) + task)
             return result
@@ -61,6 +68,32 @@ def all_reach_avoid_tasks(depth: int) -> Callable[[list[str]], list[LDBASequence
     return wrapper
 
 
+def all_reach_tasks(depth: int) -> Callable[[list[str]], list[LDBASequence]]:
+    def wrapper(propositions: list[str]) -> list[LDBASequence]:
+        reachs = [(frozenset([Assignment.single_proposition(p, propositions).to_frozen()]),
+                   frozenset())
+                  for p in propositions]
+
+        def rec(depth: int) -> list[LDBASequence]:
+            if depth == 0:
+                return []
+            if depth == 1:
+                return [(r,) for r in reachs]
+            rec_res = rec(depth - 1)
+            result = []
+            for task in rec_res:
+                next_reach = task[0][0]
+                for p, _ in reachs:
+                    if p == next_reach:
+                        continue
+                    result.append(((p, frozenset()),) + task)
+            return result
+
+        return rec(depth)
+
+    return wrapper
+
+
 if __name__ == '__main__':
-    tasks = all_reach_avoid_tasks(1)(['a', 'b', 'c'])
-    print(tasks)
+    tasks = all_reach_avoid_tasks(2)(['a', 'b', 'c', 'd'])
+    pprint(tasks)
