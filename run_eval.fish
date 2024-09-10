9d496c9311af8a040c50ca36db34cf999c368c98
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2024-09-10 17:50:05 +0100

    Removes fish scripts

--- run_eval.fish
+++
@@ -1,9 +0,0 @@
-#!/bin/env fish
-
-conda activate deepltl
-
-set seeds 1 2 3 4 5
-
-for seed in $seeds
-    PYTHONPATH=src/ python src/evaluation/eval.py --seed $seed
-end
--- run_letter.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env fish
-
-conda activate deepltl
-set device gpu
-set name stage3_095
-set num_procs 16
-PYTHONPATH=src/ ./run_letter.py --num_procs $num_procs --device $device --name $name --seed 1 --log_wandb
-PYTHONPATH=src/ ./run_letter.py --num_procs $num_procs --device $device --name $name --seed 2 --log_wandb
-PYTHONPATH=src/ ./run_letter.py --num_procs $num_procs --device $device --name $name --seed 3 --log_wandb
