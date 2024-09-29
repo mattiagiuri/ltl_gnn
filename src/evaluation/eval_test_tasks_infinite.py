fad88ed32f73e83e2174d8daabbce515d121bfe4
Mathias Jackermeier <mathias.jackermeier@outlook.com>
2024-09-29 17:23:56 +0100

    Evaluation updates

--- src/evaluation/eval_test_tasks_infinite.py
+++ src/evaluation/eval_test_tasks_infinite.py
@@ -1,10 +1,12 @@
 import os
 
 import pandas as pd
+from tqdm import tqdm
 
 from evaluation.simulate import simulate
 
 import argparse
+import multiprocessing as mp
 
 env_to_tasks = {
     'PointLtl2-v0': [
@@ -19,7 +21,7 @@ env_to_tasks = {
     ],
     'FlatWorld-v0': [
         'GF (blue & green) & GF (red & magenta)',
-        'GF red & GF yellow & GF orange & G !blue',
+        'GF (aqua & blue) & GF red & GF yellow & G !green'
     ]
 }
 
@@ -37,23 +39,29 @@ def main(env, exp):
     if os.path.exists(f'results_infinite/{env}.csv'):
         df = pd.read_csv(f'results_infinite/{env}.csv')
         results = df.values.tolist()
-    for task in tasks:
-        print(f'Running task: {task}')
-        for seed in seeds:
-            print(f'Running seed: {seed}')
+    num_procs = 8
+    with mp.Pool(num_procs) as pool:
+        for task in tasks:
+            print(f'Running task: {task}')
             # deterministic = task.startswith('GF')  # TODO
             deterministic = False
-            accepting_visits = simulate(env, gamma, exp, seed, num_episodes, task, False, False, deterministic=deterministic)
-            results.append(['DeepLTL', task, seed, accepting_visits])
-            df = pd.DataFrame(results, columns=['method', 'task', 'seed', 'accepting_visits'])
-            os.makedirs('results_infinite', exist_ok=True)
-            df.to_csv(f'results_infinite/{env}.csv', index=False)
+            args = [[env, gamma, exp, seed, num_episodes, task, False, False, deterministic] for seed in seeds]
+            for result in tqdm(pool.imap_unordered(eval_task, args), total=len(seeds)):
+                accepting_visits, seed = result
+                results.append(['DeepLTL', task, seed, accepting_visits])
+                df = pd.DataFrame(results, columns=['method', 'task', 'seed', 'accepting_visits'])
+                os.makedirs('results_infinite', exist_ok=True)
+                df.to_csv(f'results_infinite/{env}.csv', index=False)
+
+
+def eval_task(simulate_args):
+    return simulate(*simulate_args), simulate_args[3]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', type=str, choices=['PointLtl2-v0', 'LetterEnv-v0', 'FlatWorld-v0'],
-                        default='LetterEnv-v0')
-    parser.add_argument('--exp', type=str, default='deepltl')
+                        default='FlatWorld-v0')
+    parser.add_argument('--exp', type=str, default='deepset_complex')
     args = parser.parse_args()
     main(args.env, args.exp)
